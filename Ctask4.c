be0408ba820eb88ad8c3c026b84dae76c531c180
stuart tichener <76698984+stuart4404@users.noreply.github.com>
2021-04-11 10:06:42 +0100

    Add files via upload

---
+++ Ctask4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+    /*set variable names one command char forllowed by sequence of names*/
+    char a,b,c,d,e;
+
+    /*input values for variables*/
+    a = 'H';
+    b = 'e';
+    c = 'l';
+    d = 'o';
+    e = '\n';
+
+
+    /* print %c 5 commands then comma then 5 corresponding values*/
+    printf("%c%c%c%c%c",a,b,c,c,d,e);
+
+    return (0);
+
+}
