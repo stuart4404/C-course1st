6cf1400a4d783611226a7d301b75e44906d2c9b6
stuart tichener <76698984+stuart4404@users.noreply.github.com>
2021-04-12 10:10:53 +0100

    Add files via upload
    some print f code

---
+++ c coding 5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*  %d is a placeholder argument for the following input*/
+/*  \n is the new line argument*/
+
+int main()
+{
+
+    int a = 65;
+
+    printf("the value of 'a' is %d\n");
+    return 0;
+
+}
