713db169efcf25070cb8dbb72507a0f28730546c
stuart tichener <76698984+stuart4404@users.noreply.github.com>
2021-04-13 10:48:52 +0100

    Add files via upload
    print a int and constant char

---
+++ c code 7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 12;
+	const char b = 't';
+
+	printf("Here is the value of A: %d\n",a);
+	printf("Here is the value of B: %c\n",b);
+
+	return(0);
+}
+
